1ce3f4318074e3f3dff218446064fc32348e0943
waffle <lopit1219@naver.com>
2025-09-28 15:18:29 +0900

    Update and rename 설정값.py to (1) 설정값.py

--- drone-route-planner/drone_route_functions/(1) 설정값.py
+++ drone-route-planner/drone_route_functions/(1) 설정값.py
@@ -1,12 +1,13 @@
 import math
 import pandas as pd
-import folium
-import heapq
-from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+import folium  # 좌표를 지도에 표시, 경로 시각화 가능
+import heapq   # 가장 작은/큰 값 효율적 관리
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2   # Google OR-Tools - 최적화 문제 라이브러리 VRP (Vehicle Routing Problem, 차량 경로 문제) 사용
 
 # -----------------------
 # 설정값
 # -----------------------
+
 DRONE_COUNT = 3          # 드론 수
 DRONE_CAPACITY = 2       # 드론 1회 배송 가능 수
 MAX_TRIPS = 3            # 드론 1대당 최대 운행 횟수
